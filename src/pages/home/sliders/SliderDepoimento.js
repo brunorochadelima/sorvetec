4b1bc52c6df9fd4017bc3527350a1735bce573d1
Bruno Rocha de Lima <brunorocha33@live.com>
2022-07-08 14:21:02 -0300

    lazyframe

--- src/pages/home/sliders/SliderDepoimento.js
+++ src/pages/home/sliders/SliderDepoimento.js
@@ -2,6 +2,10 @@ import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
+// Carregamento lento de iframes
+import lazyframe from "lazyframe";
+
+
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
@@ -14,6 +18,7 @@ import "./sliderDepoimento.css";
 import { Autoplay, Pagination, Navigation } from "swiper";
 
 export default function SliderDepoimento() {
+  lazyframe(".lazyframe")
   return (
     <>
       <Swiper
@@ -44,19 +49,7 @@ export default function SliderDepoimento() {
         className="container"
       >
         <SwiperSlide>
-          <iframe className="swiper-lazy"
-            width="100%"
-            height="300"
-            src="https://www.youtube.com/embed/__UWtcN86ck"
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <iframe className="swiper-lazy"
+          <iframe className="swiper-lazy lazyframe"
             width="100%"
             height="300"
             src="https://www.youtube.com/embed/__UWtcN86ck"
@@ -68,7 +61,7 @@ export default function SliderDepoimento() {
         </SwiperSlide>
 
         <SwiperSlide>
-          <iframe className="swiper-lazy"
+          <iframe className="swiper-lazy lazyframe"
             width="100%"
             height="300"
             src="https://www.youtube.com/embed/__UWtcN86ck"
@@ -80,7 +73,19 @@ export default function SliderDepoimento() {
         </SwiperSlide>
 
         <SwiperSlide>
-          <iframe className="swiper-lazy"
+          <iframe className="swiper-lazy lazyframe"
+            width="100%"
+            height="300"
+            src="https://www.youtube.com/embed/__UWtcN86ck"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <iframe className="swiper-lazy lazyframe"
             width="100%"
             height="300"
             src="https://www.youtube.com/embed/__UWtcN86ck"

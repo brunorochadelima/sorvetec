d908c618b1689edffa5bd5d6634766f2c5b6b8e3
Bruno Rocha de Lima <brunorocha33@live.com>
2022-07-12 17:22:03 -0300

    ajustando card

--- src/components/cardProduto/CardProduto.jsx
+++ src/components/cardProduto/CardProduto.jsx
@@ -1,19 +1,32 @@
 import style from "./CardProduto.module.scss";
 
 export default function CardProduto(props) {
-  //função verifica se produto está com desconto para fazer a rendericação condicional
+
+  
+  // Desestruturação de propriedades
+  const {
+    Product: {
+      price,
+      promotional_price,
+      name,
+      payment_option_details,
+      ProductImage,
+    },
+  } = props;
+
+  const priceFormatado = Number(price).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+  const promotional_priceFormatado = Number(promotional_price).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+  //Função verifica se produto está com desconto para fazer a rendericação condicional dps preços
   function estaEmPromocao(props) {
-    if (props.Product.promotional_price > 0) {
+    if (promotional_price > 0) {
       return (
         <>
-          <p className={style.card__precoDe}>De: R${props.Product.price}</p>
+          <p className={style.card__precoDe}>De: {priceFormatado}</p>
           <p className={style.card__precoPor}>
             Por:{" "}
             <span>
-              R$
-              {Number(props.Product.promotional_price) > 0
-                ? props.Product.promotional_price
-                : " "}
+              {Number(promotional_price) > 0 ? promotional_priceFormatado : " "}
             </span>
           </p>
         </>
@@ -21,34 +34,33 @@ export default function CardProduto(props) {
     }
     return (
       <p className={style.card__precoPor}>
-        R$ <span>{props.Product.price}</span>{" "}
+         <span>{priceFormatado}</span>
       </p>
     );
   }
 
+  // Calcula o desconto em %
   function calculaDesconto(props) {
     return (
-      <div className={style.card__tag_desconto}> -
-      {(((props.Product.price - props.Product.promotional_price) /
-        props.Product.price) *
-      100).toFixed(2)}%
+      <div className={style.card__tag_desconto}>
+        -{(((price - promotional_price) / price) * 100).toFixed(2)}%
       </div>
     );
   }
 
   return (
     <div className={style.card}>
-      {props.Product.promotional_price > 0 && calculaDesconto(props)}
+      {promotional_price > 0 && calculaDesconto(props)}
 
-      <img src={props.Product.ProductImage[0].http} alt="" />
+      <img src={ProductImage[0].http} alt="" />
       {/* <p>ID: {props.Product.id}</p> */}
-      <h2 className={style.card__nome}>{props.Product.name}</h2>
+      <h2 className={style.card__nome}>{name}</h2>
 
       {estaEmPromocao(props)}
 
       <p>
-        ou {props.Product.payment_option_details[2].plots}X de{" "}
-        {props.Product.payment_option_details[2].value} à vista no cartão
+        ou {payment_option_details[2].plots}X de{" "}
+        {payment_option_details[2].value} à vista no cartão
       </p>
     </div>
   );
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,53 +6,61 @@ import style from "./Footer.module.scss";
 
 export default function Footer() {
   return (
-    <footer className={tema.container}>
-      <div className={style.flex_container}>
-        <div className={style.logos}>
-          <Logo />
-          <a href="https://www.multivisi.com.br/"><SeloMultivisi /></a>
-          
+    <div className={style.footer}>
+      <footer className={tema.container}>
+        <div className={style.flex_container}>
+          <div className={style.logos}>
+            <Logo />
+            <a href="https://www.multivisi.com.br/">
+              <SeloMultivisi />
+            </a>
+          </div>
+          <div>
+            <h3>Fale com a gente</h3>
+            <p>Televendas:</p>
+            <p>
+              <b>(34) 3257 0800</b>
+            </p>
+            <p>
+              Segunda a sexta 08h às 19h <br />
+              <br /> Sábado das 08h às 12h{" "}
+            </p>
+          </div>
+          <div>
+            <h3>Mapa do site</h3>
+            <p>Home</p>
+            <p>Calculadora de lucro</p>
+            <p>Sobre</p>
+            <p>Blog</p>
+            <p>Produtos</p>
+          </div>
+          <div>
+            <h3>Showrooms</h3>
+            <p>Uberlândia</p>
+            <p>São Paulo</p>
+            <p>Belo Horizonte</p>
+            <p>Recife</p>
+            <p>Goiânia</p>
+            <p>Porto Alegre</p>
+            <p>Rio de Janeiro</p>
+            <p>Fortaleza</p>
+            <p>Curitiba</p>
+            <p>Salvador</p>
+            <p>Florianópolis</p>
+            <p>Ribeirão Preto</p>
+            <p>Brasília</p>
+            <p>Campinas</p>
+            <p>Cuiabá</p>
+            <p>Londrina</p>
+            <p>São José Dos Campos</p>
+            <p>Sorocaba</p>
+            <p>Belém</p>
+            <p>Manaus</p>
+            <p>Vila Velha</p>
+            <p>Campo Grande</p>
+          </div>
         </div>
-        <div>
-          <h3>Fale com a gente</h3>
-          <p>Televendas:</p>
-          <p><b>(34) 3257 0800</b></p>
-          <p>Segunda a sexta 08h às 19h <br/><br /> Sábado das 08h às 12h </p>
-        </div>
-        <div>
-          <h3>Mapa do site</h3>
-          <p>Home</p>
-          <p>Calculadora de lucro</p>
-          <p>Sobre</p>
-          <p>Blog</p>
-          <p>Produtos</p>
-        </div>
-        <div>
-          <h3>Showrooms</h3>
-          <p>Uberlândia</p>
-          <p>São Paulo</p>
-          <p>Belo Horizonte</p>
-          <p>Recife</p>
-          <p>Goiânia</p>
-          <p>Porto Alegre</p>
-          <p>Rio de Janeiro</p>
-          <p>Fortaleza</p>
-          <p>Curitiba</p>
-          <p>Salvador</p>
-          <p>Florianópolis</p>
-          <p>Ribeirão Preto</p>
-          <p>Brasília</p>
-          <p>Campinas</p>
-          <p>Cuiabá</p>
-          <p>Londrina</p>
-          <p>São José Dos Campos</p>
-          <p>Sorocaba</p>
-          <p>Belém</p>
-          <p>Manaus</p>
-          <p>Vila Velha</p>
-          <p>Campo Grande</p>
-        </div>
-      </div>
-    </footer>
+      </footer>
+    </div>
   );
 }

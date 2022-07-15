16809ceb144132435ae434ef33a5b8ff4c9c7006
Bruno Rocha de Lima <brunorocha33@live.com>
2022-07-15 17:50:59 -0300

    header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,6 @@
-import { AppBar, Box, Container, Toolbar } from '@mui/material'
+import { AppBar, Box, Container, IconButton, InputBase, MenuItem, Paper, Toolbar } from '@mui/material'
 import { ReactComponent as Logo } from "assets/imagens/logo-sorvetec.svg";
+import { FiSearch } from 'react-icons/fi';
 import React from 'react'
 
 export default function Header() {
@@ -9,11 +10,30 @@ export default function Header() {
   return (
     <Box sx={{ flexGrow: 1}}>
       <AppBar position="static" sx={{margin: 0}}>
-      <Container maxWidth="lg">
-        <Toolbar sx={{padding: 2}}><Logo/></Toolbar>
+      <Container maxWidth="xl">
+        <Toolbar sx={{py: 2, display: 'flex', flexWrap: 'wrap' }}>
+         <Logo/>
+         <Box sx={{ justifyContent: 'center', flexGrow: 1, color: 'white', display: { xs: 'none', lg: 'flex' } }} >
+         <MenuItem>Início</MenuItem>
+         <MenuItem>Calculadora de lucro</MenuItem>
+         <MenuItem>Blog</MenuItem>
+         <MenuItem>Sobre</MenuItem>
+         <MenuItem>Máquinas</MenuItem>
+         </Box>
+
+         <Paper sx={{ display: 'flex', flexWrap: 'noWrap', mt:{xs: 2}}}>
+         <InputBase sx={{ ml: 1, backgroundColor: 'white', px: 2, py: 1, borderRadius: 2 }} placeholder="O que você procura?"
+         inputProps={{ 'aria-label': 'O que você está procurando?' }}
+         />
+         <IconButton sx={{px: 3}} type="submit" aria-label="search"><FiSearch /></IconButton>
+         </Paper>
+      
+        
+        </Toolbar>
       </Container>
       </AppBar>
     </Box>
   )
 }
 
+// https://mui.com/pt/system/properties/

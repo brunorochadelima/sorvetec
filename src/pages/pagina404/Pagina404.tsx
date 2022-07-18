999e91144783cb0d454408970b639d394800b7c1
Bruno Rocha de Lima <brunorocha33@live.com>
2022-07-18 16:04:59 -0300

    página 404

--- src/pages/pagina404/Pagina404.tsx
+++ src/pages/pagina404/Pagina404.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import img404 from 'assets/imagens/erro404.png'
+import { Button } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+import style from "./Pagina404.module.scss"
 
 export default function Pagina404() {
+
+  const navigate = useNavigate()
+  
   return (
-    <div>Pagina404</div>
+    <div className={style.container404}>
+      <img src={img404} alt="erro 404" />
+      <p>Ops! Não encontramos essa página</p>
+      <Button
+      size="large"
+      variant="contained"
+      onClick={() => navigate(-1)}
+      >‹ Voltar</Button>
+      
+    </div>
   )
 }

5bbc0e92716d0ad75a8b2d9ab1d789157c243658
Bruno Rocha de Lima <brunorocha33@live.com>
2022-07-19 10:39:55 -0300

    criando função busca

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -9,9 +9,15 @@ import Container from "@mui/material/Container";
 import MenuItem from "@mui/material/MenuItem";
 import { GrMenu } from "react-icons/gr";
 import { ReactComponent as Logo } from "assets/imagens/logo-sorvetec.svg";
-import { InputAdornment, TextField } from "@mui/material";
+import { InputAdornment, Paper, TextField } from "@mui/material";
 import { FiSearch } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import api from "api/api";
+import { useEffect, useState } from "react";
+import CardProduto from "components/cardProduto/CardProduto";
+import { style } from "@mui/system";
+import styles from "./Header.module.scss";
+import tema from "theme/Tema.module.scss";
 
 const pages = [
   {
@@ -36,7 +42,58 @@ const pages = [
   },
 ];
 
+interface IProdutos {
+  Product: any;
+  Products: any;
+}
+
 export default function Header() {
+  // Pesquisar Produto
+  const [produtosFiltrados, setProdutosFiltrados] = useState<IProdutos[]>([]);
+  const [query, setQuery] = useState("");
+  const [mostarComponente, setMostrarComponente] = useState(false);
+
+  function pesquisaProduto(evento: React.FormEvent<HTMLFormElement>) {
+    evento.preventDefault();
+    try {
+      setMostrarComponente(true);
+      api
+        .get("web_api/search", {
+          params: { category_id: 421, available: 1, query: query },
+        })
+        .then((response) => setProdutosFiltrados(response.data.Products));
+    } catch (erro) {
+      console.log(erro);
+    }
+  }
+
+  function resultadosBusca() {
+    if (mostarComponente) {
+      return (
+        <>
+          <Box sx={{ my: 2 }}>
+            <Typography variant="h6" color={"#46474B"} align="center">
+              Você buscou por: {query}
+            </Typography>
+            {produtosFiltrados.length === 0 && (
+              <Typography variant="h6" color={"orange"} align="center">
+                Ops! Não encontramos nenhum produto com o termo pesquisado
+              </Typography>
+            )}
+          </Box>
+          <div className={styles.containerBusca}>
+            {produtosFiltrados.map((produto) => (
+              <div key={produto.Product.id}>
+                <CardProduto {...produto} />
+              </div>
+            ))}
+          </div>
+        </>
+      );
+    }
+  }
+
+  // Funções menu
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
     null
   );
@@ -50,107 +107,125 @@ export default function Header() {
   };
 
   return (
-    <AppBar position="static" sx={{ mb: 2 }}>
-      <Container maxWidth="xl">
-        <Toolbar
-          disableGutters
-          sx={{
-            display: "flex",
-            flexWrap: "wrap",
-            justifyContent: "center",
-            py: 1,
-            gap: 1,
-          }}
-        >
-          {/* Logo Desktop */}
-          <Box sx={{ display: { xs: "none", md: "block" } }}>
-            <Logo style={{ padding: "15px" }} />
-          </Box>
+    <>
+      <AppBar position="static" sx={{ mb: 2 }}>
+        <Container maxWidth="xl">
+          <Toolbar
+            disableGutters
+            sx={{
+              display: "flex",
+              flexWrap: "wrap",
+              justifyContent: "center",
+              py: 1,
+              gap: 1,
+            }}
+          >
+            {/* Logo Desktop */}
+            <Box sx={{ display: { xs: "none", md: "block" } }}>
+              <Logo style={{ padding: "15px" }} />
+            </Box>
 
-          {/* Navegação Mobile */}
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              {/* <MenuIcon /> */}
-              <GrMenu />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
+            {/* Navegação Mobile */}
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                {/* <MenuIcon /> */}
+                <GrMenu />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {pages.map((page, index) => (
+                  <Link
+                    to={page.link}
+                    key={page.name}
+                    style={{ textDecoration: "none" }}
+                  >
+                    <Typography>
+                      <MenuItem>{page.name}</MenuItem>{" "}
+                    </Typography>
+                  </Link>
+                ))}
+              </Menu>
+            </Box>
+
+            {/* Logo mobile */}
+            <Box sx={{ flexGrow: 1, display: { xs: "block", md: "none" } }}>
+              <Logo />
+            </Box>
+
+            {/* Lins nav Desktop */}
+            <Box
               sx={{
-                display: { xs: "block", md: "none" },
+                flexGrow: 1,
+                display: { xs: "none", md: "flex", justifyContent: "center" },
               }}
             >
-              {pages.map((page) => (
-                <Link to={page.link} style={{ textDecoration: "none" }}>
-                  {" "}
-                  <Typography>
-                    <MenuItem key={page.name}>{page.name}</MenuItem>{" "}
+              {pages.map((page, index) => (
+                <Link
+                  to={page.link}
+                  key={page.name}
+                  style={{ textDecoration: "none" }}
+                >
+                  <Typography sx={{ color: "white" }}>
+                    <MenuItem>{page.name}</MenuItem>{" "}
                   </Typography>
                 </Link>
               ))}
-            </Menu>
-          </Box>
+            </Box>
 
-          {/* Logo mobile */}
-          <Box sx={{ flexGrow: 1, display: { xs: "block", md: "none" } }}>
-            <Logo />
-          </Box>
+            {/* Input Pesquisa */}
+            <Box
+              component="form"
+              onSubmit={pesquisaProduto}
+              sx={{ backgroundColor: "white", borderRadius: 2, flexGrow: 1 }}
+            >
+              <TextField
+                placeholder="O que você procura?"
+                type="search"
+                variant="outlined"
+                fullWidth
+                size="small"
+                onChange={(e) => setQuery(e.target.value)}
+                value={query}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <FiSearch />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
 
-          {/* Lins nav Desktop */}
-          <Box
-            sx={{
-              flexGrow: 1,
-              display: { xs: "none", md: "flex", justifyContent: "center" },
-            }}
-          >
-            {pages.map((page) => (
-              <Link to={page.link} style={{ textDecoration: "none" }}>
-                <Typography sx={{ color: "white" }}>
-                  <MenuItem key={page.name}>{page.name}</MenuItem>{" "}
-                </Typography>
-              </Link>
-            ))}
-          </Box>
-
-          {/* Input Pesquisa */}
-          <Box sx={{ backgroundColor: "white", borderRadius: 2 }}>
-            <TextField
-              placeholder="O que você procura?"
-              type="search"
-              variant="outlined"
-              fullWidth
-              size="small"
-              // onChange={handleSearchFieldOnChange}
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <FiSearch />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </Box>
-        </Toolbar>
+      <Container onClick={() => setMostrarComponente(false)}>
+        {resultadosBusca()}
       </Container>
-    </AppBar>
+    </>
   );
 }
 

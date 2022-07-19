586f7d28b5b336263fff7c28afabbb9da9df3cac
Bruno Rocha de Lima <brunorocha33@live.com>
2022-07-19 14:30:54 -0300

    divisão de código

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,36 @@
 import BotaoFlutuante from "components/BotaoFlutuante";
 import Footer from "components/footer/Footer";
 import ProdutoDetalhes from "pages/produtoDetalhes/ProdutoDetalhes";
+import { ReactComponent as IconLoading } from "assets/imagens/icon-loading.svg";
 
 import Home from "pages/home/Home";
 import Pagina404 from "pages/pagina404/Pagina404";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "components/header/Header";
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
 
-const Catalogo = lazy(() => import("pages/catalogo/Catalogo"))
+const Catalogo = lazy(() => import("pages/catalogo/Catalogo"));
 
 function App() {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/catalogo" element={<Catalogo />} />
-        <Route path="catalogo/:id" element={<ProdutoDetalhes />} />
-        <Route path="*" element={<Pagina404 />} />
-      </Routes>
-      <Footer />
-      <BotaoFlutuante />
+      <Suspense
+        fallback={
+          <div>
+            <IconLoading />
+          </div>
+        }
+      >
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/catalogo" element={<Catalogo />} />
+          <Route path="catalogo/:id" element={<ProdutoDetalhes />} />
+          <Route path="*" element={<Pagina404 />} />
+        </Routes>
+        <Footer />
+        <BotaoFlutuante />
+      </Suspense>
     </BrowserRouter>
   );
 }

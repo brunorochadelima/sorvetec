bf6313b6492d806dcddb77fb92becf606476e24c
Bruno Rocha de Lima <brunorocha33@live.com>
2022-08-09 09:40:16 -0300

    Adicionando arquivos para service worker

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 const theme = createTheme({
@@ -44,3 +45,8 @@ root.render(
   
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();

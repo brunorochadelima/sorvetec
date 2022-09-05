aa841d6677dae4b311b073faff85548d47386334
Bruno Rocha de Lima <brunorocha33@live.com>
2022-09-05 08:55:40 -0300

    estilizando cards do blog

--- src/components/cardBlog/CardBlog.tsx
+++ src/components/cardBlog/CardBlog.tsx
@@ -33,6 +33,7 @@ export default function CardBlog(props: IBlogs) {
       <img src={caminhoImagem + post_cover} alt={post_title} />
       <h2>{post_title}</h2>
       <div> {parse(`${resume} ...`)}</div>
+      <br />
       <Button variant="outlined">Continuar Lendo</Button>
     </div>
   );

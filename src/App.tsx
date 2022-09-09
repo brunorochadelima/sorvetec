522320a02c55cd92ef4af568dfcc12088d9af469
Bruno Rocha de Lima <brunorocha33@live.com>
2022-09-09 10:02:43 -0300

    criando rotas privadas

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,78 @@
 import BotaoFlutuante from "components/BotaoFlutuante";
 import Footer from "components/footer/Footer";
 import { ReactComponent as IconLoading } from "assets/imagens/icon-loading.svg";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useNavigate,
+} from "react-router-dom";
 import Header from "components/header/Header";
 import { lazy, Suspense } from "react";
 import { BuscaProvider } from "context/Busca";
 import ScrollToTop from "utils/ScrollToTop";
+import { isAuthenticated } from "pages/admin/login/auth";
 
 const Home = lazy(() => import("pages/home/Home"));
 const Catalogo = lazy(() => import("pages/catalogo/Catalogo"));
 const QuemSomos = lazy(() => import("pages/quemSomos/QuemSomos"));
 const Pagina404 = lazy(() => import("pages/pagina404/Pagina404"));
 const Busca = lazy(() => import("pages/busca/Busca"));
-const ProdutoDetalhes = lazy(() => import("pages/produtoDetalhes/ProdutoDetalhes"));
+const ProdutoDetalhes = lazy(() =>
+  import("pages/produtoDetalhes/ProdutoDetalhes")
+);
 const Calculadora = lazy(() => import("pages/calculadora/Calculadora"));
-const BlogFeed = lazy(()=> import("pages/blog/BlogFeed"));
-const BlogPost = lazy(()=> import("pages/blog/BlogPost"));
+const BlogFeed = lazy(() => import("pages/blog/BlogFeed"));
+const BlogPost = lazy(() => import("pages/blog/BlogPost"));
+const Login = lazy(() => import("pages/admin/login/Login"));
 
+
+//https://dev.to/iamandrewluca/private-route-in-react-router-v6-lg5
+
+function PrivateRoute({ children }:any) {
+  const navigate = useNavigate();
+  let auth = isAuthenticated();
+  return auth ? children : navigate("/login");
+}
 
 function App() {
   return (
     <BrowserRouter>
-    <ScrollToTop/>
+      <ScrollToTop />
       <BuscaProvider>
         <Header />
-        <Suspense fallback={<div><IconLoading /></div>}>
+        <Suspense
+          fallback={
+            <div>
+              <IconLoading />
+            </div>
+          }
+        >
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/catalogo" element={<Catalogo />} />
             <Route path="catalogo/:id" element={<ProdutoDetalhes />} />
-            <Route path="/calculadora" element={<Calculadora/>} />
+            <Route path="/calculadora" element={<Calculadora />} />
             <Route path="/busca" element={<Busca />} />
             <Route path="/quem-somos" element={<QuemSomos />} />
             <Route path="/blog" element={<BlogFeed />} />
             <Route path="blog/:id" element={<BlogPost />} />
             <Route path="*" element={<Pagina404 />} />
+            <Route path="/login" element={<Login />} />
+
+            <Route
+              path="/private"
+              element={
+                <PrivateRoute>
+                  <Catalogo />
+                </PrivateRoute>
+              }
+            />
+
           </Routes>
           <Footer />
         </Suspense>
-          <BotaoFlutuante />
+        <BotaoFlutuante />
       </BuscaProvider>
     </BrowserRouter>
   );
---
+++ src/pages/admin/login/Login.tsx
@@ -0,0 +1,88 @@
+import tema from "theme/Base.module.scss";
+import { Button, Paper, TextField, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import axios from "axios";
+import { useState } from "react";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [erro, setErro] = useState("");
+  const [token, setToken] = useState("");
+
+
+  // Criar função pra ver se o usuário esta autenticado dentro de um compoente separado auth
+  // Guardar o token no local storage
+  // Criar componente PrivateRoute que retorna <Route/>
+
+  function aoSubmeterForm(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    axios
+      .post("https://www.sorvetec.com.br/laravel/public/api/login", {
+        email: email,
+        password: password,
+      })
+      .then((response) => {
+        console.log(response);
+        setToken(response.data.plainTextToken);
+      })
+      .catch((error) => {
+        console.log(error);
+        setErro(error.response.data);
+      });
+  }
+
+  return (
+    <div style={{ backgroundColor: "#F3F4F6" }}>
+      <div className={tema.container}>
+        <Box sx={{ display: "grid", placeItems: "center", height: "80vh" }}>
+          <Paper sx={{ width: { md: "430px" } }}>
+            <Box sx={{ p: 4 }}>
+              <p className={tema.titulo_h2}>
+                <b>BLOG </b>ADMIN
+              </p>
+              <h2>Entrar</h2>
+              <p>Digite seu e-mail e senha para entrar!</p>
+            </Box>
+
+            <Box component="form" onSubmit={aoSubmeterForm} sx={{ px: 2 }}>
+              <TextField
+                label="Email"
+                variant="outlined"
+                margin="dense"
+                fullWidth
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <TextField
+                label="Senha"
+                variant="outlined"
+                margin="dense"
+                fullWidth
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+
+              {/* Mensagem erro */}
+              <Typography variant="h6" color={"red"}>
+                {erro === "Unauthorized" ? "Email e/ou senha inválidos" : ""}
+              </Typography>
+
+              <Button
+                variant="contained"
+                size="large"
+                fullWidth
+                type="submit"
+                sx={{ my: 4 }}
+              >
+                Entrar
+              </Button>
+            </Box>
+          </Paper>
+        </Box>
+      </div>
+    </div>
+  );
+}

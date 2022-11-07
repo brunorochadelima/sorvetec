4cbfb327c7570cf8dd3c2fdefda7cfdb4174e44d
Bruno Rocha de Lima <brunorocha33@live.com>
2022-11-07 13:32:45 -0300

    removerdo service worker

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import { HelmetProvider } from "react-helmet-async";
 
 const theme = createTheme({
@@ -54,4 +53,4 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+
